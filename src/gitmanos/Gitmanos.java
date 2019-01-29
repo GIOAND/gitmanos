0b97621834d05a7aa18514366fc7938410287503
GIOAND <46938851+GIOAND@users.noreply.github.com>
2019-01-29 12:50:19 -0600

    Gitmanos.java
    AGREGE OTRO PRINTF

--- src/gitmanos/Gitmanos.java
+++ src/gitmanos/Gitmanos.java
@@ -16,6 +16,7 @@ public class Gitmanos {
       {
          System.out.print("Sumando 1 : ");
          System.out.printf("hola bety");
+          System.out.printf("hola GIO");
          int s1 = Integer.parseInt(br.readLine());
          System.out.print("Sumando 2 : ");
          int s2 = Integer.parseInt(br.readLine());
